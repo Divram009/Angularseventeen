d008e5be035671771b47370eeaa224ea61184de7
divram009 <divyasri707@gmail.com>
2024-06-06 16:29:02 -0400

    3rd checkin

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { ContentProjectionComponent } from "../content-projection/content-projection.component";
 
 @Component({
-  selector: 'app-angular',
-  standalone: true,
-  imports: [],
-  templateUrl: './angular.component.html',
-  styleUrl: './angular.component.css'
+    selector: 'app-angular',
+    standalone: true,
+    templateUrl: './angular.component.html',
+    styleUrl: './angular.component.css',
+    imports: [RouterOutlet, ReactiveFormsModule, CommonModule, ContentProjectionComponent]
 })
 export class AngularComponent {
 
+  userForm: FormGroup;
+  isSubmitted:boolean=false;
+  constructor() {
+  this.userForm=new FormGroup({
+
+    firstName:new FormControl("",[Validators.required,Validators.minLength(5)]),
+    lastName:new FormControl("",[Validators.required, Validators.minLength(4)]),
+    userName:new FormControl("",[Validators.required, Validators.email]),
+    city:new FormControl("",Validators.required),
+    state:new FormControl("",[Validators.required]),
+    zipcode:new FormControl("",Validators.required),
+    isAgree:new FormControl("",Validators.required),
+  });
+}
+
+onStateChange()
+{
+  var state=this.userForm.controls['state'].value;
+  var city=this.userForm.controls['city'].value;
+  if(city=="media" && state!='PA')
+  {
+    this.userForm.controls['state'].setErrors({'invalidstate':true});
+  }
+  else{
+    this.userForm.controls['state'].setErrors({'invalidstate':false});
+  }
+
+}
+onSubmit(){
+  this.onStateChange();
+
+//  var stateValue=this.userForm.controls['state'].value;
+//  if(stateValue==''){
+//   this.userForm.controls['state'].setErrors({'required':true});
+//  } else{
+//   this.userForm.controls['state'].setErrors({'required':false});
+//   }
+  this.isSubmitted=true;
+  const isValid=this.userForm.valid;
+  console.log(this.userForm)
+  }
 }
--- src/app/content-projection/content-projection.component.ts
+++ src/app/content-projection/content-projection.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-content-projection',
@@ -11,24 +12,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './content-projection.component.css'
 })
 export class ContentProjectionComponent {
-
-  userForm: FormGroup;
-  isSubmitted:boolean=false;
-  constructor() {
-  this.userForm=new FormGroup({
-
-    firstName:new FormControl("",[Validators.required,Validators.minLength(5)]),
-    lastName:new FormControl("",[Validators.required, Validators.minLength(4)]),
-    userName:new FormControl("",[Validators.required, Validators.email]),
-    city:new FormControl(""),
-    state:new FormControl(""),
-    zipcode:new FormControl(""),
-    isAgree:new FormControl(""),
-  });
-}
-onSubmit(){
-this.isSubmitted=true;
-  const isValid=this.userForm.valid;
-}
-
+  @Input() label:string | undefined;
+  @Input() tooltip:string|undefined;
+  @Input() validation: any;
 }
